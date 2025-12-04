00138bff756ed2fd5d653ce260f3e704252bdda1
osmar-cruz <a01715556@tec.mx>
2025-12-03 22:51:47 -0600

    Update class_project_roulette.cpp
    here is almost the same

--- class_project_roulette.cpp
+++ class_project_roulette.cpp
@@ -4,22 +4,24 @@
 
 using namespace std;
 
-class Roulette {                             //This is the class roulette
+class Roulette {  // Class representing the roulette wheel
 private:
     int lastNumber;
 
 public:
     Roulette() {
-        srand(time(0));   // random seed
+        srand(time(0));  // Seed the random number generator once when the roulette is created
         lastNumber = 0;
     }
 
+    // Spins the wheel and returns a random number between 0 and 36
     int spin() {
-        lastNumber = rand() % 37; // 0 - 36
+        lastNumber = rand() % 37;
         return lastNumber;
     }
 
-    int getLastNumber() {
+    // Returns the last number that was spun
+    int getLastNumber() const {
         return lastNumber;
     }
 };
