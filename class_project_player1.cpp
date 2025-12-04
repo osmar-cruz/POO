5a9d2374da2f1c5211786a9ac6d0c1bed8b3f3e5
osmar-cruz <a01715556@tec.mx>
2025-12-03 22:51:01 -0600

    Update class_project_player1.cpp
    now we can have more than 1 player

--- class_project_player1.cpp
+++ class_project_player1.cpp
@@ -1,38 +1,67 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 
 using namespace std;
 
-class Player {                        //This is the class player
+// Base class Person to represent a generic person with a name
+class Person {
 private:
     string name;
+
+public:
+    // Constructor
+    Person(string n) : name(n) {}
+
+    // Getter for name
+    string getName() const {
+        return name;
+    }
+
+    // Setter for name
+    void setName(string n) {
+        name = n;
+    }
+};
+
+// Player class inherits from Person and adds balance and betting behavior
+class Player : public Person {
+private:
     double balance;
 
 public:
-    Player(string n, double b) {
-        name = n;
-        balance = b;
-    }
+    // Constructor takes a name and starting balance, and calls the base Person constructor
+    Player(string n, double b) : Person(n), balance(b) {}
 
-    double getBalance() {
+    // Returns the current balance
+    double getBalance() const {
         return balance;
     }
 
-    void showBalance() {
-        cout << "Balance: $" << balance << endl;
+    // Displays the player's name and current balance
+    void showBalance() const {
+        cout << getName() << "'s balance: $" << balance << endl;
     }
 
+    // Attempts to deduct the bet amount from balance (if sufficient funds)
+    // Returns true if the bet is placed, false if there's not enough balance
     bool betMoney(double amount) {
         if (amount > balance) {
-            cout << "Not enough balance." << endl;
+            cout << getName() << " doesn't have enough balance for that bet." << endl;
             return false;
         }
         balance -= amount;
         return true;
     }
 
+    // Adds money to the player's balance (used for winnings payouts)
     void addMoney(double amount) {
         balance += amount;
     }
+};
+
+        balance += amount;
+    }
+
 };
